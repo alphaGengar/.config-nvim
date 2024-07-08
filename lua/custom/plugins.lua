4309069448a3aeeab96f20be47ad4b02fb25aa71
Swaraj Adhikari <swaraj2065@gmail.com>
2024-07-07 22:46:11 -0700

    fire.nvim fraud

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,15 +1,4 @@
 local lazy_plugins = {
-  -- Leetcode
-  {
-    'glacambre/firenvim',
-
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    lazy = not vim.g.started_by_firenvim,
-    build = function()
-        vim.fn["firenvim#install"](0)
-    end
-  },
   -- Presence
   {
     "jiriks74/presence.nvim",
